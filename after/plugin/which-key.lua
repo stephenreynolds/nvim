9520339163f6171f1ca75e4f3dd8e207b7a4afaa
Stephen Reynolds <mail@stephenreynolds.dev>
2022-12-23 19:14:25 -0500

    Add a bunch of new keymaps

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -12,11 +12,44 @@ wk.register({
         h = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
         l = { "<cmd>BufferLineCloseRight<cr>", "Close all to the right" },
         D = { "<cmd>BufferLineSortByDirectory<cr>", "Sort by directory" },
-        L = { "<cmd>BufferLineSortByExtension<cr>", "Sort by language" }
+        L = { "<cmd>BufferLineSortByExtension<cr>", "Sort by language" },
+        p = { "<cmd>BufferLineTogglePin<cr>", "Toggle pin" }
     },
     c = { "<cmd>bd<cr>", "Close buffer" },
+    d = "Delete",
+    y = "Copy to clipboard",
+    Y = "Copy line to clipboard",
     f = "Find file",
+    g = {
+        name = "Git",
+        g = { "<cmd>Git<cr>", "" },
+        d = { "<cmd>Git diff<cr>", "Diff" },
+        l = { "<cmd>git blame<cr>", "Blame" }
+    },
     h = { "<cmd>nohl<cr", "No highlight" },
+    l = {
+        name = "LSP",
+        a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+        d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+        w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+        f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
+        i = { "<cmd>LspInfo<cr>", "Info" },
+        I = { "<cmd>Mason<cr>", "Mason Info" },
+        j = { vim.diagnostic.goto_next, "Next Diagnostic" },
+        k = { vim.diagnostic.goto_prev, "Prev Diagnostic" },
+        l = { vim.lsp.codelens.run, "CodeLens Action" },
+        q = { vim.diagnostic.setloclist, "Quickfix" },
+        r = { vim.lsp.buf.rename, "Rename" },
+        R = { vim.lsp.buf.references, "References" },
+        s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+        S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
+        e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" }
+    },
+    N = {
+        name = "Neovim",
+        c = {
+        }
+    },
     p = {
         name = "Packer",
         c = { "<cmd>PackerCompile<cr>", "Compile" },
@@ -25,8 +58,26 @@ wk.register({
         S = { "<cmd>PackerStatus<cr>", "Status" },
         u = { "<cmd>PackerUpdate<cr>", "Update" }
     },
-    u = "Undo tree",
+    r = {
+        name = "Replace",
+        w = "Replace word"
+    },
+    s = {
+        name = "Search",
+        b = { "<cmd>Telescope git_branches<cr>", "Checkout" },
+        c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+        f = { "<cmd>Telescope find_files<cr>", "Fine File" },
+        h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+        H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+        M = { "<cmd>Telescope man_pages<cr>", "Manpages" },
+        r = { "<cmd>Telescope oldfiles<cr>", "Open recent file" },
+        R = { "<cmd>Telescope registers<cr>", "Registers" },
+        t = { "<cmd>Telescope live_grep<cr>", "Text" },
+        k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+        C = { "<cmd>Telescope commands<cr>", "Commands" },
+        p = { "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>", "Colorscheme with Preview" }
+ ,   },
+    u = { "<cmd>UndotreeToggle | UndotreeFocus<cr>", "Undo tree" },
     w = { "<cmd>w<cr>", "Save" },
     q = { "<cmd>q<cr>", "Quit" }
 }, { prefix = "<leader>" })
-
--- lua/kamicha/remap.lua
+++ lua/kamicha/remap.lua
@@ -1,2 +1,34 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move selection in and out of code blocks.
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+
+-- Keep cursor in place when joining lines.
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor centered vertically when jumping half page.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor centered vertically when jumping between search results.
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste over and delete selection, instead of copying the selection.
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Delete to the void register
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Make Q do nothing
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Replace all instances of a word in the buffer.
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
