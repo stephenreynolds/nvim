5c9b7656733d1ba938670e28525f06ffe8347dfe
Stephen Reynolds <mail@stephenreynolds.dev>
2022-12-23 19:10:41 -0500

    Show buffer close icon on hover

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,6 +1,11 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{
     options = {
-        diagnostics = "nvim_lsp"
+        diagnostics = "nvim_lsp",
+        hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { "close" }
+        }
     }
 }
