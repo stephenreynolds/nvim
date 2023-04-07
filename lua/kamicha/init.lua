cd36d5bd7ed55ef57d3a171f6f56b093e5e79f38
Stephen Reynolds <mail@stephenreynolds.dev>
2023-04-07 07:46:08 -0400

    Formatting

--- lua/kamicha/init.lua
+++ lua/kamicha/init.lua
@@ -5,21 +5,22 @@ require("kamicha.packer")
 vim.api.nvim_create_autocmd("ColorScheme", {
     pattern = "*",
     callback = function()
-      local hl_groups = {
-        "Normal",
-        "SignColumn",
-        "NormalNC",
-        "TelescopeBorder",
-        "NvimTreeNormal",
-        "EndOfBuffer",
-        "MsgArea",
-      }
-      for _, name in ipairs(hl_groups) do
-        vim.cmd(string.format("highlight %s ctermbg=none guibg=none", name))
-      end
+        local hl_groups = {
+            "Normal",
+            "SignColumn",
+            "NormalNC",
+            "TelescopeBorder",
+            "NvimTreeNormal",
+            "EndOfBuffer",
+            "MsgArea",
+        }
+        for _, name in ipairs(hl_groups) do
+            vim.cmd(string.format("highlight %s ctermbg=none guibg=none", name))
+        end
     end,
-  })
-  vim.opt.fillchars = "eob: "
+})
+vim.opt.fillchars = "eob: "
+
 vim.api.nvim_exec([[
     augroup highlight_yank
     autocmd!
