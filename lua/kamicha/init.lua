7f4c4dc4a921a0b13c9c313d665ef92bbd91a08f
Stephen Reynolds <mail@stephenreynolds.dev>
2023-04-07 23:44:56 -0400

    Add settings for netrw

--- lua/kamicha/init.lua
+++ lua/kamicha/init.lua
@@ -28,3 +28,9 @@ vim.api.nvim_exec([[
 ]], false)
 
 vim.api.nvim_command("autocmd TextYankPost * silent! lua vim.highlight.on_yank({higroup='Visual', timeout=200})")
+-- Use the current window to open a file in netrw
+vim.g.netrw_browse_split = 0
+-- Hide the netrw banner
+vim.g.netrw_banner = 0
+-- Set the width of the netrw window
+vim.g.netrw_winsize = 25
