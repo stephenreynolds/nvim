7bb621e0d9176dc2a8de5a530ef48cf6727219c0
Stephen Reynolds <mail@stephenreynolds.dev>
2023-04-08 00:17:17 -0400

    Remove harpoon background

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,3 +8,12 @@ vim.keymap.set("n", "<C-left>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-down>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-up>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-right>", function() ui.nav_file(4) end)
+
+vim.api.nvim_create_autocmd({ "Filetype" }, {
+  pattern = "harpoon",
+  callback = function()
+    vim.opt.cursorline = true
+    vim.api.nvim_set_hl(0, 'HarpoonWindow', { link = 'Normal' })
+    vim.api.nvim_set_hl(0, 'HarpoonBorder', { link = 'Normal' })
+  end
+})
