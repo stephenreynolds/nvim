3a434252b42bc9f90fd6352b4d6d7ec50e1f0f77
Stephen Reynolds <mail@stephenreynolds.dev>
2023-04-09 03:34:08 -0400

    Use fugitive to for branch and gitsigns for diff in lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,3 +1,14 @@
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed
+    }
+  end
+end
+
 require("lualine").setup {
     options = {
         disabled_filetypes = {
@@ -7,6 +18,11 @@ require("lualine").setup {
         globalstatus = true
     },
     sections = {
-        lualine_c = { {"filename", path = 1} }
+        lualine_a = { "mode" },
+        lualine_b = { {"FugitiveHead", icon = ""}, {"diff", source = diff_source}, "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
     }
 }
