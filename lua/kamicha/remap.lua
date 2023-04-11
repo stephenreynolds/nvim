48e95356fe511bb4c5f917b881e6c3c5cc3d959a
Stephen Reynolds <mail@stephenreynolds.dev>
2023-04-11 05:05:39 -0400

    Add keymap to open file explorer

--- lua/kamicha/remap.lua
+++ lua/kamicha/remap.lua
@@ -53,3 +53,6 @@ end, { desc = "Source file" })
 
 -- Terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal" })
+
+-- Open file explorer
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { desc = "Open file explorer" })
