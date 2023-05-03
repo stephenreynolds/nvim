c79fb0e7684935ae44ceaa4b55e5027d5d054223
Stephen Reynolds <mail@stephenreynolds.dev>
2023-05-03 09:11:00 -0400

    Use telescope 0.1.x

--- lua/kamicha/packer.lua
+++ lua/kamicha/packer.lua
@@ -29,8 +29,7 @@ return require("packer").startup(function(use)
 	-- Telescope: Highly extendable fuzzy finder.
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		-- or                            , branch = "0.1.x",
+		tag = "0.1.x",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
