51e9caf229f3959fc49cad16b136a3e2bfae343b
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-15 22:40:50 -0400

    Add nvim-ufo

---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,36 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+    event = "BufReadPost",
+	config = function()
+		vim.o.foldcolumn = "0"
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
+		local language_servers = require("lspconfig").util.available_servers()
+		for _, ls in ipairs(language_servers) do
+			require("lspconfig")[ls].setup({
+				capabilities = capabilities,
+			})
+		end
+		require("ufo").setup()
+	end,
+	keys = {
+		{
+			"zR",
+			"<cmd>lua require('ufo').openAllFolds()<cr>",
+			desc = "Open all folds",
+		},
+		{
+			"zM",
+			"<cmd>lua require('ufo').closeAllFolds()<cr>",
+			desc = "Close all folds",
+		},
+	},
+}
