b8ea5cc51b3e92803fbd7e5915419576c857dbfc
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-15 20:35:04 -0400

    Add treesj

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,8 @@
+return {
+	"Wansmer/treesj",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	keys = {
+		{ "<space>b", "<cmd>TSJToggle<cr>", desc = "Toggle code block" },
+	},
+	opts = {},
+}
