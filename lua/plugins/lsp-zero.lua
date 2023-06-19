240b0bde0fba2cef3e19f1088c3402ea1ab95844
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-18 21:50:57 -0400

    Add Intellij-like cmp mapping

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -46,6 +46,19 @@ return {
 			["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 			["<C-y>"] = cmp.mapping.confirm({ select = true }),
 			["<C-Space>"] = cmp.mapping.complete(),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+				if cmp.visible() then
+					local entry = cmp.get_selected_entry()
+					if not entry then
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+					else
+						cmp.confirm()
+					end
+				else
+					fallback()
+				end
+			end, { "i", "s", "c" }),
 		})
 
 		cmp.setup.cmdline("/", {
