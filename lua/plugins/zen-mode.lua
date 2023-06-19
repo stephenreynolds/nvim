ef930b7bffb341856da806e1ea770fd0a85363d7
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-18 23:02:52 -0400

    Increate width of buffer in zen mode to 126

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,17 +1,16 @@
 -- zen-mode: Distraction free writing.
 return {
     "folke/zen-mode.nvim",
+    opts = {
+        window = {
+            width = 126,
+        },
+    },
     keys = {
         {
             "<leader>z",
             function()
                 local zen = require("zen-mode")
-                zen.setup {
-                    window = {
-                        width = 90,
-                        options = {}
-                    }
-                }
                 zen.toggle()
                 vim.wo.wrap = false
                 vim.wo.number = true
