be9f6dcaac91c222975bb81e81a4437ba391df76
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-20 09:32:00 -0400

    Add jumb keybinds for luasnip

--- lua/plugins/editing/luasnip.lua
+++ lua/plugins/editing/luasnip.lua
@@ -1,12 +1,37 @@
 return {
-	"L3MON4D3/LuaSnip",
-	version = "1.*",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-	},
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp",
-	config = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
-	end,
+    "L3MON4D3/LuaSnip",
+    version = "1.*",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+    },
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+    keys = {
+        {
+            "<tab>",
+            function()
+                return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+            end,
+            expr = true,
+            silent = true,
+            mode = "i",
+        },
+        {
+            "<tab>",
+            function()
+                require("luasnip").jump(1)
+            end,
+            mode = "s",
+        },
+        {
+            "<s-tab>",
+            function()
+                require("luasnip").jump(-1)
+            end,
+            mode = { "i", "s" },
+        },
+    },
 }
