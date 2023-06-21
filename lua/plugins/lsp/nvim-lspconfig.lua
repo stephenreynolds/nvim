e6af79fbbcae820865e4de6e764401aedb283531
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 09:22:11 -0400

    Update lspconfig

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,21 +1,29 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	cmd = "LspInfo",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local function lspSymbol(name, icon)
-			local hl = "DiagnosticSign" .. name
-			vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-		end
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    lazy = false,
+    config = function()
+        local function lspSymbol(name, icon)
+            local hl = "DiagnosticSign" .. name
+            vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+        end
 
-		lspSymbol("Error", "󰅙")
-		lspSymbol("Info", "󰋼")
-		lspSymbol("Hint", "󰌵")
-		lspSymbol("Warn", "")
-	end,
+        lspSymbol("Error", "󰅙")
+        lspSymbol("Info", "󰋼")
+        lspSymbol("Hint", "󰌵")
+        lspSymbol("Warn", "")
+
+        vim.diagnostic.config({
+            virtual_text = {
+                prefix = "",
+            },
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+        })
+    end,
 }
