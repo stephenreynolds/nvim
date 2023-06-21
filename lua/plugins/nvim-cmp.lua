25da5395fda65e2a4c3beac2631e6caa527db757
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 07:57:30 -0400

    Formatting

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,101 +1,105 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-cmdline",
-        "saadparwaiz1/cmp_luasnip",
-        "petertriho/cmp-git",
-        "onsails/lspkind.nvim",
-        "L3MON4D3/LuaSnip",
-    },
-    event = "InsertEnter",
-    config = function()
-        local cmp = require("cmp")
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        local lspkind = require("lspkind")
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
+		"petertriho/cmp-git",
+		"onsails/lspkind.nvim",
+		"L3MON4D3/LuaSnip",
+	},
+	event = "InsertEnter",
+	config = function()
+		local cmp = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local lspkind = require("lspkind")
 
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = "symbol_text",
-                    preset = "default",
-                    maxwidth = 50,
-                    ellipsis_char = "...",
-                    before = function(_, vim_item)
-                        return vim_item
-                    end,
-                    -- Show source next to items
-                    -- menu = {
-                    -- 	buffer = "[buf]",
-                    -- 	nvim_lsp = "[LSP]",
-                    -- 	nvim_lua = "[api]",
-                    -- 	path = "[path]",
-                    -- 	luasnip = "[snip]",
-                    -- 	gh_issues = "[issues]",
-                    -- },
-                }),
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-                    if cmp.visible() then
-                        local entry = cmp.get_selected_entry()
-                        if not entry then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                        else
-                            cmp.confirm()
-                        end
-                    else
-                        fallback()
-                    end
-                end, { "i", "s", "c" }),
-                ["<C-u>"] = cmp.mapping.scroll_docs(-5),
-                ["<C-d>"] = cmp.mapping.scroll_docs(5),
-            }),
-            sources = cmp.config.sources({
-                { name = "copilot" },
-                { name = "gh_issues" },
-                { name = "nvim_lua" },
-                { name = "nvim_lsp" },
-                { name = "path" },
-                { name = "cmp_cmdline" },
-                { name = "luasnip" },
-                { name = "buffer",     keyword_length = 5 },
-            }),
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            sorting = {
-                priority_weight = 2,
-                comparators = {
-                    cmp.config.compare.offset,
-                    cmp.config.compare.exact,
-                    cmp.config.compare.score,
-                    cmp.config.compare.recently_used,
-                    cmp.config.compare.locality,
-                    cmp.config.compare.kind,
-                    cmp.config.compare.sort_text,
-                    cmp.config.compare.length,
-                    cmp.config.compare.order,
-                },
-            },
-        })
-    end,
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					preset = "default",
+					maxwidth = 50,
+					ellipsis_char = "...",
+					before = function(_, vim_item)
+						return vim_item
+					end,
+					-- Show source next to items
+					-- menu = {
+					-- 	buffer = "[buf]",
+					-- 	nvim_lsp = "[LSP]",
+					-- 	nvim_lua = "[api]",
+					-- 	path = "[path]",
+					-- 	luasnip = "[snip]",
+					-- 	gh_issues = "[issues]",
+					-- },
+				}),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+					if cmp.visible() then
+						local entry = cmp.get_selected_entry()
+						if not entry then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						else
+							cmp.confirm()
+						end
+					else
+						fallback()
+					end
+				end, { "i", "s", "c" }),
+				["<cr>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
+			}),
+			sources = cmp.config.sources({
+				{ name = "copilot" },
+				{ name = "gh_issues" },
+				{ name = "nvim_lua" },
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+				{ name = "cmp_cmdline" },
+				{ name = "luasnip" },
+				{ name = "buffer", keyword_length = 5 },
+			}),
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			sorting = {
+				priority_weight = 2,
+				comparators = {
+					cmp.config.compare.offset,
+					cmp.config.compare.exact,
+					cmp.config.compare.score,
+					cmp.config.compare.recently_used,
+					cmp.config.compare.locality,
+					cmp.config.compare.kind,
+					cmp.config.compare.sort_text,
+					cmp.config.compare.length,
+					cmp.config.compare.order,
+				},
+			},
+		})
+	end,
 }
