4fb70f430e0affbd6148d9981330411dcd0a99b6
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 00:29:48 -0400

    Remove ColorScheme autocmd

--- lua/autocommands/init.lua
+++ lua/autocommands/init.lua
@@ -1,26 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
-autocmd("ColorScheme", {
-	pattern = "*",
-	callback = function()
-		local hl_groups = {
-			"Normal",
-			"NormalFloat",
-			"SignColumn",
-			"NormalNC",
-			"TelescopeNormal",
-			"TelescopeBorder",
-			"NvimTreeNormal",
-			"EndOfBuffer",
-			"MsgArea",
-		}
-		for _, name in ipairs(hl_groups) do
-			vim.cmd(string.format("highlight %s ctermbg=none guibg=none", name))
-		end
-	end,
-})
-
 -- Highlight on yank
 local yank_group = augroup("HighlightYank", {})
 autocmd("TextYankPost", {
