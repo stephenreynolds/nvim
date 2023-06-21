bb7d6e6e5c23ee55d5d25265f38c2290c9337bff
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 07:59:02 -0400

    Configure lua_ls for nvim

--- lua/plugins/lsp/servers/lua_ls.lua
+++ lua/plugins/lsp/servers/lua_ls.lua
@@ -1,5 +1,17 @@
 return {
-	Lua = {
-		hint = { enable = true },
-	},
+    Lua = {
+        diagnostics = {
+            globals = { "vim" },
+        },
+        hint = { enable = true },
+        workspace = {
+            library = {
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
+            },
+            maxPreload = 100000,
+            preloadFileSize = 10000,
+        },
+    },
 }
