af6c87513f27a61b11b4b569db959da55f09ea09
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 05:56:30 -0400

    Set number width

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,12 @@
+-- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.numberwidth = 2
 
 vim.opt.laststatus = 3
 
 vim.opt.clipboard = "unnamedplus"
-
--- Cursor
-vim.opt.cursorline = true
 
 -- Indentation
 vim.opt.smartindent = true
