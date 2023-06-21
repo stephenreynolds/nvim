c12e73f902cde5c06c679585367a4dacb70a0840
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 05:49:14 -0400

    Add some comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,18 +1,19 @@
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.laststatus = 3
 
 vim.opt.clipboard = "unnamedplus"
 
+-- Cursor
 vim.opt.cursorline = true
 
+-- Indentation
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
