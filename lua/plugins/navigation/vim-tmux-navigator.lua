f87fbc2b710501b47c8133428e46762ec40f8ae7
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 07:48:44 -0400

    Fix missing bracket

--- lua/plugins/navigation/vim-tmux-navigator.lua
+++ lua/plugins/navigation/vim-tmux-navigator.lua
@@ -1,7 +1,7 @@
 return {
     "christoomey/vim-tmux-navigator",
     keys = {
-        { "<C-h>", "cmd>TmuxNavigateLeft<cr>" },
+        { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
         { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
         { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
         { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
