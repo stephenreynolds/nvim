7d4f1082d6bfe211c82a13e829da4ed7a3c9ab71
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 00:50:04 -0400

    Fix no-neck-pain config

--- lua/plugins/ui/no-neck-pain.lua
+++ lua/plugins/ui/no-neck-pain.lua
@@ -1,40 +1,36 @@
 return {
-    "shortcuts/no-neck-pain.nvim",
-    version = "*",
-    opts = {
-            width = 120,
-            buffers = {
-                backgroundColor = "catppuccin",
-                blend = 0.3,
-                scratchPad = {
-                    enabled = true,
-                    location = "~/Documents/",
-                },
-                bo = {
-                    filetype = "md",
-                },
-            },
-    },
-    keys = {
-        {
-            "<leader>zz",
-            "<cmd>NoNeckPain<cr>",
-            desc = "Center buffer",
-        },
-        {
-            "<leader>zr",
-            ":NoNeckPainResize ",
-            desc = "Resize buffer width",
-        },
-        {
-            "<leader>zu",
-            "<cmd>NoNeckPainWidthUp<cr>",
-            desc = "Increase buffer width",
-        },
-        {
-            "<leader>zd",
-            "<cmd>NoNeckPainWidthDown<cr>",
-            desc = "Decrease buffer width",
-        },
-    },
+	"shortcuts/no-neck-pain.nvim",
+	version = "*",
+	opts = {
+		width = 120,
+		disableOnLastBuffer = true,
+		killAllBuffersOnDisable = true,
+		autocmds = {
+			enableOnVimEnter = true,
+			enableOnTabEnter = true,
+			reloadOnColorSchemeChange = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>zz",
+			"<cmd>NoNeckPain<cr>",
+			desc = "Center buffer",
+		},
+		{
+			"<leader>zr",
+			":NoNeckPainResize ",
+			desc = "Resize buffer width",
+		},
+		{
+			"<leader>zu",
+			"<cmd>NoNeckPainWidthUp<cr>",
+			desc = "Increase buffer width",
+		},
+		{
+			"<leader>zd",
+			"<cmd>NoNeckPainWidthDown<cr>",
+			desc = "Decrease buffer width",
+		},
+	},
 }
