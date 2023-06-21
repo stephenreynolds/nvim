77c78e2ddbed7905a19caba6b0003ef3c6ac76d7
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-21 08:07:33 -0400

    Configure telescope

--- lua/plugins/navigation/telescope.lua
+++ lua/plugins/navigation/telescope.lua
@@ -3,6 +3,42 @@ return {
 	"nvim-telescope/telescope.nvim",
 	version = "0.1.x",
 	depencencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		defaults = {
+			vimgrep_arguments = {
+				"rg",
+				"-L",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+			},
+
+			prompt_prefix = "   ",
+			selection_caret = "  ",
+			entry_prefix = "  ",
+			initial_mode = "insert",
+			selection_strategy = "reset",
+			sorting_strategy = "ascending",
+
+			layout_strategy = "horizontal",
+			layout_config = {
+				horizontal = {
+					prompt_position = "top",
+					preview_width = 0.55,
+					results_width = 0.8,
+				},
+				vertical = {
+					mirror = false,
+				},
+				width = 0.87,
+				height = 0.80,
+				preview_cutoff = 120,
+			},
+		},
+	},
 	keys = {
 		{
 			"<leader>f",
