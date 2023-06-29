fb719860065f44bc6cfa086985b176a2fa93515f
Stephen Reynolds <mail@stephenreynolds.dev>
2023-06-29 17:57:47 -0400

    Fix lsp settings

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,103 +1,102 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "neovim/nvim-lspconfig",
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    opts = {
-        ensure_installed = {
-            "tsserver",
-            "eslint",
-            "lua_ls",
-            "rust_analyzer",
-            "clangd",
-            "gopls",
-            "golangci_lint_ls",
-            "emmet_ls",
-            "jsonls",
-            "html",
-            "marksman",
-            "sqlls",
-            "taplo",
-            "tailwindcss",
-            "yamlls",
-            "dockerls",
-            "docker_compose_language_service",
-        },
-        automatic_installation = true,
-    },
-    config = function(_, opts)
-        local mason_lspconfig = require("mason-lspconfig")
-        require("mason").setup()
-        mason_lspconfig.setup(opts)
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	opts = {
+		ensure_installed = {
+			"tsserver",
+			"eslint",
+			"lua_ls",
+			"rust_analyzer",
+			"clangd",
+			"gopls",
+			"golangci_lint_ls",
+			"emmet_ls",
+			"jsonls",
+			"html",
+			"marksman",
+			"sqlls",
+			"taplo",
+			"tailwindcss",
+			"yamlls",
+			"dockerls",
+			"docker_compose_language_service",
+		},
+		automatic_installation = true,
+	},
+	config = function(_, opts)
+		local mason_lspconfig = require("mason-lspconfig")
+		require("mason").setup()
+		mason_lspconfig.setup(opts)
 
-        local lspconfig = require("lspconfig")
-        local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lspconfig = require("lspconfig")
+		local default_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                local settings = {}
-                if server_name == "lua_ls" then
-                    settings = require("plugins.lsp.servers.lua_ls")
-                elseif server_name == "tsserver" then
-                    settings = require("plugins.lsp.servers.tsserver")
-                elseif server_name == "gopls" then
-                    settings = require("plugins.lsp.servers.gopls")
-                elseif server_name == "clangd" then
-                    settings = require("plugins.lsp.servers.clangd")
-                end
+		local default_on_attach = function(client, bufnr)
+			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-                local function on_attach(_, bufnr)
-                    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+			if vim.lsp.buf.inlay_hint and client.server_capabilities.inlayHintProvider then
+				vim.lsp.buf.inlay_hint(bufnr, true)
+			end
 
-                    vim.keymap.set(
-                        "n",
-                        "<leader>li",
-                        "<cmd>LspInfo<cr>",
-                        { buffer = bufnr, remap = false, desc = "Info" }
-                    )
-                    vim.keymap.set("n", "<leader>lh", function()
-                        vim.lsp.buf.inlay_hint(bufnr)
-                    end, { buffer = bufnr, remap = false, desc = "Toggle inlay hints" })
-                    vim.keymap.set("n", "<leader>lf", function()
-                        vim.lsp.buf.format()
-                    end, { buffer = bufnr, remap = false, desc = "Format" })
-                    vim.keymap.set("n", "gd", function()
-                        vim.lsp.buf.definition()
-                    end, { buffer = bufnr, remap = false, desc = "Go to definition" })
-                    vim.keymap.set("n", "K", function()
-                        vim.lsp.buf.hover()
-                    end, { buffer = bufnr, remap = false, desc = "Hover" })
-                    vim.keymap.set("n", "<leader>ld", function()
-                        vim.diagnostic.open_float()
-                    end, { buffer = bufnr, remap = false, desc = "Open diagnostics popup" })
-                    vim.keymap.set("n", "<leader>[d", function()
-                        vim.diagnostic.goto_prev()
-                    end, { buffer = bufnr, remap = false, desc = "Go to previous diagnostic" })
-                    vim.keymap.set("n", "<leader>]d", function()
-                        vim.diagnostic.goto_next()
-                    end, { buffer = bufnr, remap = false, desc = "Go to next diagnostic" })
-                    vim.keymap.set("n", "<leader>la", function()
-                        vim.lsp.buf.code_action()
-                    end, { buffer = bufnr, remap = false, desc = "Code action" })
-                    vim.keymap.set("n", "<leader>ll", function()
-                        vim.lsp.codelens.run()
-                    end, { buffer = bufnr, remap = false, desc = "CodeLens" })
-                    vim.keymap.set("n", "<leader>rn", function()
-                        vim.lsp.buf.rename()
-                    end, { buffer = bufnr, remap = false, desc = "Rename symbol" })
-                    vim.keymap.set("i", "<C-h>", function()
-                        vim.lsp.buf.signature_help()
-                    end, { buffer = bufnr, remap = false, desc = "Signature help" })
-                end
+			vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<cr>", { buffer = bufnr, remap = false, desc = "Info" })
+			vim.keymap.set("n", "<leader>lh", function()
+				vim.lsp.buf.inlay_hint(bufnr)
+			end, { buffer = bufnr, remap = false, desc = "Toggle inlay hints" })
+			vim.keymap.set("n", "<leader>lf", function()
+				vim.lsp.buf.format()
+			end, { buffer = bufnr, remap = false, desc = "Format" })
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+				vim.lsp.buf.hover()
+			end, { buffer = bufnr, remap = false, desc = "Hover" })
+			vim.keymap.set("n", "<leader>ld", function()
+				vim.diagnostic.open_float()
+			end, { buffer = bufnr, remap = false, desc = "Open diagnostics popup" })
+			vim.keymap.set("n", "<leader>[d", function()
+				vim.diagnostic.goto_prev()
+			end, { buffer = bufnr, remap = false, desc = "Go to previous diagnostic" })
+			vim.keymap.set("n", "<leader>]d", function()
+				vim.diagnostic.goto_next()
+			end, { buffer = bufnr, remap = false, desc = "Go to next diagnostic" })
+			vim.keymap.set("n", "<leader>la", function()
+				vim.lsp.buf.code_action()
+			end, { buffer = bufnr, remap = false, desc = "Code action" })
+			vim.keymap.set("n", "<leader>ll", function()
+				vim.lsp.codelens.run()
+			end, { buffer = bufnr, remap = false, desc = "CodeLens" })
+			vim.keymap.set("n", "<leader>rn", function()
+				vim.lsp.buf.rename()
+			end, { buffer = bufnr, remap = false, desc = "Rename symbol" })
+			vim.keymap.set("i", "<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end, { buffer = bufnr, remap = false, desc = "Signature help" })
+		end
 
-                lspconfig[server_name].setup({
-                    capabilities = lsp_capabilities,
-                    settings = settings,
-                    on_attach = on_attach,
-                })
-            end,
-        })
-    end,
+		lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+			capabilities = default_capabilities,
+			on_attach = default_on_attach,
+		})
+
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				lspconfig[server_name].setup({})
+			end,
+			["clangd"] = function()
+				lspconfig.clangd.setup(require("plugins.lsp.servers.clangd"))
+			end,
+			["gopls"] = function()
+				lspconfig.gopls.setup(require("plugins.lsp.servers.gopls"))
+			end,
+			["lua_ls"] = function()
+				lspconfig.lua_ls.setup(require("plugins.lsp.servers.lua_ls"))
+			end,
+			["tsserver"] = function()
+				lspconfig.tsserver.setup(require("plugins.lsp.servers.tsserver"))
+			end,
+		})
+	end,
 }
