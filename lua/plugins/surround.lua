0ead0015952434ac83ab0ed64b899144c7243c44
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 19:09:38 -0400

    plugin(surround): add surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,7 +1,7 @@
 return {
     "kylechui/nvim-surround",
     version = "*",
-    event = { "BufRead", "BufWinEnter", "BufNewFile" },
+    event = { "VeryLazy" },
     opts = {
         keymaps = {
             insert = "<C-g>s",
