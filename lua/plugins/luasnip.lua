ff0b02d5b490506a458869740da208839883fa59
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 19:04:47 -0400

    plugin(luasnip): add luasnip

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,42 @@
+return {
+    "L3MON4D3/LuaSnip",
+    version = "1.*",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+    },
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    opts = {
+        history = true,
+        updateevents = "TextChanged,TextChangedI",
+    },
+    config = function(_, opts)
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip").setup(opts)
+    end,
+    keys = {
+        {
+            "<tab>",
+            function()
+                return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+            end,
+            expr = true,
+            silent = true,
+            mode = "i",
+        },
+        {
+            "<tab>",
+            function()
+                require("luasnip").jump(1)
+            end,
+            mode = "s",
+        },
+        {
+            "<s-tab>",
+            function()
+                require("luasnip").jump(-1)
+            end,
+            mode = { "i", "s" },
+        },
+    },
+}
