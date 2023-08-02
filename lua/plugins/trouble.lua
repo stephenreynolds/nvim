15749e8d380457af75aa4fd303de3c3e2abe10a3
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 19:01:12 -0400

    plugin(trouble): add trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,48 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"<leader>lq",
+			"<cmd>TroubleToggle quickfix<cr>",
+			silent = true,
+			noremap = true,
+			desc = "Quickfix",
+		},
+		{
+			"<leader>lr",
+			"<cmd>TroubleToggle lsp_references<cr>",
+			buffer = bufnr,
+			remap = false,
+			desc = "References",
+		},
+		{
+			"gr",
+			"<cmd>TroubleToggle lsp_references<cr>",
+			buffer = bufnr,
+			remap = false,
+			desc = "References",
+		},
+		{
+			"<leader>lD",
+			"<cmd>TroubleToggle document_diagnostics<cr>",
+			buffer = bufnr,
+			remap = false,
+			desc = "Document diagnostics",
+		},
+		{
+			"<leader>lwd",
+			"<cmd>TroubleToggle workspace_diagnostics<cr>",
+			buffer = bufnr,
+			remap = false,
+			desc = "Workspace diagnostics",
+		},
+		{
+			"<leader>lL",
+			"<cmd>TroubleToggle loclist<cr>",
+			buffer = bufnr,
+			remap = false,
+			desc = "Location list",
+		},
+	},
+}
