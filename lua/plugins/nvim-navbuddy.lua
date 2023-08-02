4d78f691e86578a8f9216ecdfb6b00f34faadad4
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 18:28:54 -0400

    plugin(nvim-navbuddy): add nvim-navbuddy

---
+++ lua/plugins/nvim-navbuddy.lua
@@ -0,0 +1,22 @@
+return {
+	"SmiteshP/nvim-navbuddy",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"SmiteshP/nvim-navic",
+		"MunifTanjim/nui.nvim",
+        "numToStr/Comment.nvim",
+        "nvim-telescope/telescope.nvim",
+	},
+	opts = {
+		lsp = {
+			auto_attach = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>ls",
+			"<cmd>Navbuddy<cr>",
+			desc = "Symbol outline",
+		},
+	},
+}
