b6f3c2ce36ef37842b2d07ab9c8cfc068857c796
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 15:23:31 -0400

    config(keymaps): move selection out of block

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,9 @@ vim.g.mapleader = " "
 -- Map write and quite from leader
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Write" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+
+-- Move selection in and out of blocks
+vim.keymap.set("v", "<S-down>", ":m '>+1<cr>gv=gv", { desc = "Move down out of block, silent = true" })
+vim.keymap.set("v", "<S-j>", ":m '>+1<cr>gv=gv", { desc = "Move down out of block, silent = true" })
+vim.keymap.set("v", "<S-up>", ":m '<-2<cr>gv=gv", { desc = "Move up out of block, silent = true" })
+vim.keymap.set("v", "<S-k>", ":m '<-2<cr>gv=gv", { desc = "Move up out of block, silent = true" })
