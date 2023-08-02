8504e71cd524c88416ab46b2ab66e17e3b3b3624
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 15:19:32 -0400

    config(keymaps): map write and quit from leader

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,5 @@
 vim.g.mapleader = " "
+
+-- Map write and quite from leader
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Write" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
