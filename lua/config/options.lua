f3e2b12f160eb0083251d7b5e4ed94d6e77b4cda
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 14:41:15 -0400

    config(options): configure search highlight

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,3 +20,6 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
