44cb0487dddc93b6f464da452033b6588ea515b5
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 17:55:21 -0400

    plugin(leap): add leap

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,8 @@
+return {
+    "ggandor/leap.nvim",
+    dependencies = { "tpope/vim-repeat" },
+    config = function()
+        require("leap").add_default_mappings()
+    end,
+    keys = { "s", "S" },
+}
