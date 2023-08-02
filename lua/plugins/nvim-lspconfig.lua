32c9807b2e09c4b3541be1f61a0494b073c3822d
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-02 18:04:24 -0400

    plugin(nvim-cmp): add nvim-cmp

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,29 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    event = "BufEnter",
+    config = function()
+        local function lspSymbol(name, icon)
+            local hl = "DiagnosticSign" .. name
+            vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+        end
+
+        lspSymbol("Error", "󰅙")
+        lspSymbol("Info", "󰋼")
+        lspSymbol("Hint", "󰌵")
+        lspSymbol("Warn", "")
+
+        vim.diagnostic.config({
+            virtual_text = {
+                prefix = "",
+            },
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+        })
+    end,
+}
