3b9469db733d449c011963eb85d52682cb4a4a66
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-08 22:24:41 -0400

    Add go to context keybind

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -1,4 +1,13 @@
 return {
-	"nvim-treesitter/nvim-treesitter-context",
+    "nvim-treesitter/nvim-treesitter-context",
     event = "BufEnter",
+    keys = {
+        {
+            "[c",
+            function() require("treesitter-context").go_to_context() end,
+            mode = "n",
+            silent = true,
+            desc = "Go to context",
+        },
+    },
 }
