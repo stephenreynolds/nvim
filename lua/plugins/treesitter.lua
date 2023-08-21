41303cecdcdbb5d81782e05c1450ecea406f9686
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-21 12:59:31 -0400

    treesitter: ensure some important parsers are installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,13 @@ return {
     build = ":TSUpdate",
     opts = {
         -- A list of parser names, or "all"
-        ensure_installed = {},
+        ensure_installed = {
+            "regex",
+            "markdown_inline",
+            "vim",
+            "lua",
+            "c",
+        },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
