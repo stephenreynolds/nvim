b0bd948798ecb56273d70846d908a454b710dc28
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-27 14:46:14 -0400

    fix: get sqlite path from envvar

--- lua/plugins/telescope-frecency.lua
+++ lua/plugins/telescope-frecency.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = { "kkharji/sqlite.lua" },
 	event = "VimEnter",
 	config = function()
+        vim.g.sqlite_clib_path = os.getenv("NVIM_SQLITE_PATH") .. "/lib/libsqlite3.so";
 		require("telescope").load_extension("frecency")
 	end,
 }
