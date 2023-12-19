a45a489ef8b6beea78bafe883a2a8bd10133d07b
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-19 18:33:55 -0500

    feat(nix): add nix flake

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "My Neovim config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      genSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+
+      pkgs = genSystems (system: import nixpkgs { inherit system; });
+
+      runtimeDependencies = genSystems (system: with pkgs.${system}; [
+        gcc
+        clang-tools
+        gnumake
+        fzf
+        unzip
+        sqlite
+        luajitPackages.sqlite
+        wget
+        go
+        cargo
+        luarocks
+        tree-sitter
+        nil
+        nixpkgs-fmt
+        lua-language-server
+        stylua
+        vscode-langservers-extracted
+        nodePackages.typescript-language-server
+        nodePackages.bash-language-server
+        haskell-language-server
+        prettierd
+        isort
+        black
+        emmet-ls
+        pyright
+        marksman
+        ocamlPackages.ocaml-lsp
+        ocamlformat
+        nixpkgs-fmt
+        rustfmt
+        rust-analyzer
+        shellcheck
+        yamlfmt
+        jq
+        codespell
+        lldb
+      ]);
+    in
+    {
+      inherit runtimeDependencies;
+
+      homeManagerModules.default = import ./nix/hm-module.nix self;
+
+      devShells = genSystems (system: {
+        default = pkgs.${system}.mkShell {
+          buildInputs = runtimeDependencies.${system};
+        };
+      });
+    };
+}
---
+++ nix/hm-module.nix
@@ -0,0 +1,47 @@
+self: { config, pkgs, ... }:
+
+let
+  system = pkgs.stdenv.hostPlatform.system;
+in
+{
+  config = {
+    programs.neovim = {
+      withNodeJs = true;
+      extraPackages = self.runtimeDependencies.${system};
+    };
+
+    home.sessionVariables = { NVIM_SQLITE_PATH = pkgs.sqlite.out; };
+
+    xdg.configFile.nvim.source = ../.;
+
+    xdg.desktopEntries = {
+      nvim = {
+        name = "Neovim";
+        genericName = "Text Editor";
+        comment = "Edit text files";
+        exec = "nvim %F";
+        icon = "nvim";
+        mimeType = [
+          "text/english"
+          "text/plain"
+          "text/x-makefile"
+          "text/x-c++hdr"
+          "text/x-c++src"
+          "text/x-chdr"
+          "text/x-csrc"
+          "text/x-java"
+          "text/x-moc"
+          "text/x-pascal"
+          "text/x-tcl"
+          "text/x-tex"
+          "application/x-shellscript"
+          "text/x-c"
+          "text/x-c++"
+        ];
+        terminal = true;
+        type = "Application";
+        categories = [ "Utility" "TextEditor" ];
+      };
+    };
+  };
+}
