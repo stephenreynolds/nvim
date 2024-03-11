2270b8140a1196b1e20d22d81cb02eb8bfff7878
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-10 21:45:56 -0400

    feat(harpoon): change some settings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,7 +2,33 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
+  opts = {
+    settings = {
+      sync_on_ui_close = true,
+    },
+  },
+  config = function(_, opts)
+    local harpoon = require('harpoon')
+    harpoon:setup(opts)
+
+    -- Keymaps for opening files in splits or tabs
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set('n', '<C-v>', function()
+          harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set('n', '<C-x>', function()
+          harpoon.ui:select_menu_item({ split = true })
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set('n', '<C-t>', function()
+          harpoon.ui:select_menu_item({ tabedit = true })
+        end, { buffer = cx.bufnr })
+      end,
+    })
+
+    -- Highlight cursor line
     vim.api.nvim_create_autocmd({ 'Filetype' }, {
       pattern = 'harpoon',
       callback = function()
@@ -25,7 +51,11 @@ return {
       '<C-e>',
       function()
         local harpoon = require('harpoon')
-        harpoon.ui:toggle_quick_menu(harpoon:list())
+        harpoon.ui:toggle_quick_menu(harpoon:list(), {
+          border = 'rounded',
+          title_pos = 'center',
+          ui_width_ratio = 0.4,
+        })
       end,
       desc = 'Harpoon quick menu',
     },
