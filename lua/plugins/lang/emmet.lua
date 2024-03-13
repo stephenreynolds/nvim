707f4c5e7c0ecbe7b25a0a2d9c3bff1ae759246b
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-12 23:04:02 -0400

    lang(emmet): ensure emmet-ls installed

--- lua/plugins/lang/emmet.lua
+++ lua/plugins/lang/emmet.lua
@@ -1,31 +1,41 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      emmet_ls = {
-        filetypes = {
-          "css",
-          "eruby",
-          "html",
-          "javascript",
-          "javascriptreact",
-          "less",
-          "sass",
-          "scss",
-          "svelte",
-          "pug",
-          "typescriptreact",
-          "vue",
-        },
-        init_options = {
-          html = {
-            options = {
-              ["bem.enabled"] = true,
-              ["jsx.enabled"] = true,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "css",
+            "eruby",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "less",
+            "sass",
+            "scss",
+            "svelte",
+            "pug",
+            "typescriptreact",
+            "vue",
+          },
+          init_options = {
+            html = {
+              options = {
+                ["bem.enabled"] = true,
+                ["jsx.enabled"] = true,
+              },
             },
           },
         },
       },
     },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "emmet-ls" })
+    end,
   },
 }
