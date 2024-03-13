2b0b8a5f0e0b49b9877b6eef54d30f3fd36b86ff
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-12 23:37:14 -0400

    lang(html): add html langauge server

---
+++ lua/plugins/lang/html.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        html = {},
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "html-lsp" })
+    end,
+  },
+}
