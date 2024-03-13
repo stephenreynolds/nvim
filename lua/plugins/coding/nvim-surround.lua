a0d26608907410456bdea76cec9ded0783763aa7
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-13 05:46:43 -0400

    plugin(nvim-surround): replace mini.surround with nvim-surround

---
+++ lua/plugins/coding/nvim-surround.lua
@@ -0,0 +1,20 @@
+return {
+  "kylechui/nvim-surround",
+  version = "*",
+  event = "VeryLazy",
+  opts = {
+    keymaps = {
+      insert = "<C-g>s",
+      insert_line = "<C-g>S",
+      normal = "s",
+      normal_cur = "ss",
+      normal_line = "S",
+      normal_cur_line = "SS",
+      visual = "S",
+      visual_line = "gS",
+      delete = "ds",
+      change = "cs",
+      change_line = "cS",
+    }
+  },
+}
