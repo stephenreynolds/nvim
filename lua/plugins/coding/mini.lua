c853806b5c2629349ae793cf1f46f1052b57c38b
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-13 05:21:21 -0400

    plugin(mini.surround): set keymaps

--- lua/plugins/coding/mini.lua
+++ lua/plugins/coding/mini.lua
@@ -9,5 +9,19 @@ return {
 
   {
     "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa",            -- Add surrounding in Normal and Visual modes
+        delete = "ds",         -- Delete surrounding
+        find = "sf",           -- Find surrounding (to the right)
+        find_left = "sF",      -- Find surrounding (to the left)
+        highlight = "sh",      -- Highlight surrounding
+        replace = "cs",        -- Replace surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+
+        suffix_last = "l",     -- Suffix to search with "prev" method
+        suffix_next = "n",     -- Suffix to search with "next" method
+      },
+    },
   },
 }
