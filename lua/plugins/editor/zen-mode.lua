4bda71755244bdfa6336f58dac39f6c0a6c51349
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-15 22:16:29 -0400

    plugin(zen-mode): add keybinds to close buffer and zen mode in one command

--- lua/plugins/editor/zen-mode.lua
+++ lua/plugins/editor/zen-mode.lua
@@ -1,5 +1,11 @@
 return {
   "folke/zen-mode.nvim",
+  opts = {
+    on_open = function(win)
+      vim.keymap.set("n", "ZZ", "<cmd>ZenMode|x!<cr>", { noremap = true, silent = true })
+      vim.keymap.set("n", "ZQ", "<cmd>ZenMode|q!<cr>", { noremap = true, silent = true })
+    end,
+  },
   keys = {
     {
       "<leader>Tz",
