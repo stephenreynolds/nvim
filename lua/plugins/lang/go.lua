bd1a7f7a4c1adf30778b279de791bab98984752e
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-15 22:14:50 -0400

    fix(lang/go): make sure gofumpt and goimports are installed

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -120,4 +120,13 @@ return {
       },
     },
   },
+
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "gofumpt", "goimports" })
+    end,
+  },
 }
