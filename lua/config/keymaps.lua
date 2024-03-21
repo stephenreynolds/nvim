55c2f3ae39f221884b294ccf02b5e2348eabac98
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-21 10:48:31 -0400

    fix(keymap): fix resize keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,11 +32,10 @@ map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Resize window using <ctrl> arrow keys
--- FIXME: Resize keymaps don't work
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<A-S-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<A-S-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<A-S-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<A-S-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Scroll buffer
 map("n", "<Up>", "<C-y>", { desc = "Scroll up" })
