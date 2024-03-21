87609135873bb5cbfe265ed5be4ca4e49a89c24c
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-21 08:12:26 -0400

    plugin(session-lens): add session-lens

--- lua/plugins/editor/auto-session.lua
+++ lua/plugins/editor/auto-session.lua
@@ -1,19 +1,39 @@
 return {
-  "rmagatti/auto-session",
-  lazy = false,
-  opts = {
-    log_level = "error",
-    auto_restore_enabled = true,
-    auto_session_enable_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
-    auto_session_use_git_branch = true,
-    cwd_change_handling = {
-      post_cwd_changed_hook = function()
-        require("lualine").refresh()
-      end,
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+    opts = {
+      log_level = "error",
+      auto_restore_enabled = true,
+      auto_session_enable_last_session = vim.loop.cwd() == vim.loop.os_homedir(),
+      auto_session_use_git_branch = true,
+      cwd_change_handling = {
+        post_cwd_changed_hook = function()
+          require("lualine").refresh()
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("auto-session").setup(opts)
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+    end,
+  },
+
+  {
+    "rmagatti/session-lens",
+    dependencies = {
+      "rmagatti/auto-session",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      previewer = true,
+    },
+    keys = {
+      {
+        "<leader>sS",
+        "<cmd>SearchSession<cr>",
+        desc = "Sessions",
+      },
     },
   },
-  config = function(_, opts)
-    require("auto-session").setup(opts)
-    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-  end,
 }
