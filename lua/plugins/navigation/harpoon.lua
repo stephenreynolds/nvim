660c232ece7890d4b507114e298a597f27ff092a
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-07 23:18:47 -0400

    plugin(harpoon): change keymaps to quick switch buffers

--- lua/plugins/navigation/harpoon.lua
+++ lua/plugins/navigation/harpoon.lua
@@ -1,91 +1,133 @@
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     settings = {
       sync_on_ui_close = true,
     },
   },
   config = function(_, opts)
-    local harpoon = require('harpoon')
+    local harpoon = require("harpoon")
     harpoon:setup(opts)
 
     -- Keymaps for opening files in splits or tabs
     harpoon:extend({
       UI_CREATE = function(cx)
-        vim.keymap.set('n', '<C-v>', function()
+        vim.keymap.set("n", "<C-v>", function()
           harpoon.ui:select_menu_item({ vsplit = true })
         end, { buffer = cx.bufnr })
 
-        vim.keymap.set('n', '<C-x>', function()
+        vim.keymap.set("n", "<C-x>", function()
           harpoon.ui:select_menu_item({ split = true })
         end, { buffer = cx.bufnr })
 
-        vim.keymap.set('n', '<C-t>', function()
+        vim.keymap.set("n", "<C-t>", function()
           harpoon.ui:select_menu_item({ tabedit = true })
         end, { buffer = cx.bufnr })
       end,
     })
 
     -- Highlight cursor line
-    vim.api.nvim_create_autocmd({ 'Filetype' }, {
-      pattern = 'harpoon',
+    vim.api.nvim_create_autocmd({ "Filetype" }, {
+      pattern = "harpoon",
       callback = function()
         vim.opt.cursorline = true
-        vim.api.nvim_set_hl(0, 'HarpoonWindow', { link = 'Normal' })
-        vim.api.nvim_set_hl(0, 'HarpoonBorder', { link = 'Normal' })
+        vim.api.nvim_set_hl(0, "HarpoonWindow", { link = "Normal" })
+        vim.api.nvim_set_hl(0, "HarpoonBorder", { link = "Normal" })
       end,
     })
   end,
   keys = {
     {
-      '<leader>a',
+      "<leader>a",
       function()
-        require('harpoon'):list():append()
-        print('Added to harpoon.')
+        require("harpoon"):list():append()
+        print("Added to harpoon.")
       end,
-      desc = 'Add to harpoon',
+      desc = "Add to harpoon",
     },
     {
-      '<C-e>',
+      "<C-e>",
       function()
-        local harpoon = require('harpoon')
+        local harpoon = require("harpoon")
         harpoon.ui:toggle_quick_menu(harpoon:list(), {
-          border = 'rounded',
-          title_pos = 'center',
+          border = "rounded",
+          title_pos = "center",
           ui_width_ratio = 0.4,
         })
       end,
-      desc = 'Harpoon quick menu',
+      desc = "Harpoon quick menu",
     },
     {
-      '<C-left>',
+      "<C-1>",
       function()
-        require('harpoon'):list():select(1)
+        require("harpoon"):list():select(1)
       end,
-      desc = 'Go to quick file 1',
+      desc = "Go to quick file 1",
     },
     {
-      '<C-down>',
+      "<C-2>",
       function()
-        require('harpoon'):list():select(2)
+        require("harpoon"):list():select(2)
       end,
-      desc = 'Go to quick file 2',
+      desc = "Go to quick file 2",
     },
     {
-      '<C-up>',
+      "<C-3>",
       function()
-        require('harpoon'):list():select(3)
+        require("harpoon"):list():select(3)
       end,
-      desc = 'Go to quick file 3',
+      desc = "Go to quick file 3",
     },
     {
-      '<C-right>',
+      "<C-4>",
       function()
-        require('harpoon'):list():select(4)
+        require("harpoon"):list():select(4)
       end,
-      desc = 'Go to quick file 4',
+      desc = "Go to quick file 4",
+    },
+    {
+      "<C-5>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Go to quick file 5",
+    },
+    {
+      "<C-6>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Go to quick file 6",
+    },
+    {
+      "<C-7>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Go to quick file 7",
+    },
+    {
+      "<C-8>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Go to quick file 8",
+    },
+    {
+      "<C-9>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Go to quick file 9",
+    },
+    {
+      "<C-0>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Go to quick file 10",
     },
   },
 }
