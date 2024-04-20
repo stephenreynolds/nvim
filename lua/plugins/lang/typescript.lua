5596a1f2238778a06827613ca8b667bd9a07aff4
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-20 15:20:54 -0400

    lang(typescript): ensure typescript-language-server installed

--- lua/plugins/lang/typescript.lua
+++ lua/plugins/lang/typescript.lua
@@ -9,6 +9,15 @@ return {
   },
 
   {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "typescript-language-server" })
+    end,
+  },
+
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
