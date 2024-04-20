905bb555bcdd2c61b62f0c5276f0afa8a24354e6
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-20 15:20:37 -0400

    linting: ensure eslint-lsp installed

--- lua/plugins/linting/eslint.lua
+++ lua/plugins/linting/eslint.lua
@@ -20,4 +20,13 @@ return {
       },
     },
   },
+
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "eslint-lsp" })
+    end,
+  },
 }
