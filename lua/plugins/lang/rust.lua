49d72cf820dfa98b3d28415698edf3b4ad77f452
Stephen Reynolds <mail@stephenreynolds.dev>
2024-06-01 14:18:07 -0400

    fix(lang/rust): remove unused crates.nvim src opt

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -88,11 +88,6 @@ return {
       {
         "Saecki/crates.nvim",
         event = { "BufRead Cargo.toml" },
-        opts = {
-          src = {
-            cmp = { enabled = true },
-          },
-        },
       },
     },
     opts = function(_, opts)
