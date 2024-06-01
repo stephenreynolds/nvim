087c8b179ab4a95b924e066e95437aab235b3776
Stephen Reynolds <mail@stephenreynolds.dev>
2024-06-01 13:31:01 -0400

    lang(rust): use <localleader> to access rust-specific keymaps

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -14,10 +14,10 @@ return {
     opts = {
       server = {
         on_attach = function(_, bufnr)
-          vim.keymap.set("n", "<leader>cR", function()
+          vim.keymap.set("n", "<localleader>a", function()
             vim.cmd.RustLsp("codeAction")
           end, { desc = "Code Action", buffer = bufnr })
-          vim.keymap.set("n", "<leader>dr", function()
+          vim.keymap.set("n", "<localleader>d", function()
             vim.cmd.RustLsp("debuggables")
           end, { desc = "Rust debuggables", buffer = bufnr })
         end,
