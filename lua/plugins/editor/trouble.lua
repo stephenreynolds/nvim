c6859404857189bbc4961ec3af906d7e4253f09e
Stephen Reynolds <mail@stephenreynolds.dev>
2024-08-13 10:43:01 -0400

    fix(trouble): fix trouble keymaps

--- lua/plugins/editor/trouble.lua
+++ lua/plugins/editor/trouble.lua
@@ -2,35 +2,30 @@ return {
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    cmd = { "TroubleToggle", "Trouble" },
+    cmd = { "Trouble" },
+    opts = {},
     keys = {
       {
         "<leader>xr",
-        "<cmd>TroubleToggle lsp_references<cr>",
+        "<cmd>Trouble lsp_references toggle<cr>",
         remap = false,
         desc = "References",
       },
       {
         "gr",
-        "<cmd>TroubleToggle lsp_references<cr>",
+        "<cmd>Trouble lsp_references toggl>",
         remap = false,
         desc = "References",
       },
       {
         "<leader>xD",
-        "<cmd>TroubleToggle document_diagnostics<cr>",
+        "<cmd>Trouble diagnostics toggle<cr>",
         remap = false,
         desc = "Document diagnostics",
       },
       {
-        "<leader>xwd",
-        "<cmd>TroubleToggle workspace_diagnostics<cr>",
-        remap = false,
-        desc = "Workspace diagnostics",
-      },
-      {
         "<leader>xl",
-        "<cmd>TroubleToggle loclist<cr>",
+        "<cmd>Trouble loclist toggle<cr>",
         remap = false,
         desc = "Location list",
       },
