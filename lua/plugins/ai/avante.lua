e54d13b6bf172e21103faf62b0083164fd863fdd
Stephen Reynolds <mail@stephenreynolds.dev>
2024-08-29 20:43:57 -0400

    chore: update dependencies

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -3,10 +3,24 @@ return {
   event = "VeryLazy",
   build = "make",
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
     {
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
