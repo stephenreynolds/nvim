7e38df386128d05057e5c32f332ce4f913696020
Stephen Reynolds <mail@stephenreynolds.dev>
2024-12-12 08:55:43 -0500

    lazy: don't notify on config change

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -26,9 +26,9 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- Automatically check for plugin updates
-  checker = { enabled = true },
+  change_detection = {
+    notify = false,
+  },
   -- Configure Lazy UI
   ui = {
     border = "rounded",
