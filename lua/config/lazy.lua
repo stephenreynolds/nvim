55c8c516a03f06cff2e35e435699755bc5b6bc92
Stephen Reynolds <mail@stephenreynolds.dev>
2024-12-12 09:58:23 -0500

    Add nvim-autopairs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { import = "plugins.coding" },
     { import = "plugins.lang" },
   },
   defaults = {
