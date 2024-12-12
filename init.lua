ef2ba82311b2df69f20f65b94e0a473be4adabc5
Stephen Reynolds <mail@stephenreynolds.dev>
2024-12-11 20:40:00 -0500

    Add mini statusline

--- init.lua
+++ init.lua
@@ -27,9 +27,7 @@ require("lazy").setup({
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
+  -- Automatically check for plugin updates
   checker = { enabled = true },
   -- Configure Lazy UI
   ui = {
