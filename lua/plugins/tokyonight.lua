c18a3c05e606567e72bef0207603fb57b251cac9
Stephen Reynolds <mail@stephenreynolds.dev>
2024-12-11 20:20:24 -0500

    Add lazy

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,29 @@
+return {
+  "folke/tokyonight.nvim",
+  enabled = true,
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night",
+    transparent = true,
+    terminal_colors = false,
+    styles = {
+      comments = { italic = false },
+      keywords = { italic = false },
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    lualine_bold = true,
+    on_highlights = function(hl)
+      hl.LspInlayHint = {
+        bg = nil,
+        fg = hl.LspInlayHint.fg,
+      }
+    end,
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd.hi("Comment gui=none")
+    vim.cmd.colorscheme("tokyonight")
+  end,
+}
