0e0b03789dc90c1c666bf6bb3d0b050d6a06fd92
Stephen Reynolds <mail@stephenreynolds.dev>
2025-05-13 20:34:13 -0400

    Remove blade treesitter parser
    No longer needed

--- lua/plugins/lang/php.lua
+++ lua/plugins/lang/php.lua
@@ -2,19 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      vim.filetype.add({ pattern = { [".*%.blade%.php"] = "blade" } })
-
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "php", "php_only", "blade", "html", "css", "javascript" })
       end
