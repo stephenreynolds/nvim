ea08ed599f832e9de9ebfff1ca2dc5b51cee5b3d
Stephen Reynolds <mail@stephenreynolds.dev>
2025-05-24 23:12:53 -0400

    lang(zig): add zig config

---
+++ lua/plugins/lang/zig.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "zig" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        zls = {},
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "lawrence-laz/neotest-zig",
+    },
+    opts = {
+      adapters = {
+        ["neotest-zig"] = {},
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "zls" })
+    end,
+  },
+}
