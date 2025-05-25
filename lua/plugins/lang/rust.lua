43d950da031cc5c81ba8414f1e7375660440a56f
Stephen Reynolds <mail@stephenreynolds.dev>
2025-05-24 23:13:01 -0400

    lang(rust): ensure taplo installed

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -155,7 +155,7 @@ return {
     optional = true,
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "codelldb" })
+      vim.list_extend(opts.ensure_installed, { "codelldb", "taplo" })
     end,
   },
 
